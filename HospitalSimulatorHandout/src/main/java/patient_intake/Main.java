d73974473508c23a9b7811d23142eb4bef289d17
NotReallyUnknown <padgdom@gmail.com>
2026-09-16 00:58:36 -0500

    Update Main.java

--- HospitalSimulatorHandout/src/main/java/patient_intake/Main.java
+++ HospitalSimulatorHandout/src/main/java/patient_intake/Main.java
@@ -19,8 +19,23 @@ public class Main {
             String[] parts = line.split(",");
 
             // TODO REQUIRED: Parse the fields from parts.
+            String patientID = parts[0];
+            String fullName = parts[1];
+            int age = Integer.parseInt(parts[2]);
+            String chiefComplaint = parts[3];
+            int triagelevel = Integer.parseInt(parts[4]);
+            String currentStage = parts[5];
+            String assignedRoom = parts[6];
+            int arrivalHour = Integer.parseInt(parts[7]);
+            String insuranceID = parts[8];
             // TODO REQUIRED: Split the full name into firstName and lastName.
+
+            String name[] = fullName.split(" ");
+            String firstName = name[1];
+            String lastName = name[2];
+            
             // TODO REQUIRED: Create a Patient and add it to patients.
+            
          }
 
          // TODO REQUIRED: Display the completed registry.
